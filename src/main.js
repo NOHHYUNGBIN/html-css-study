469ffcf47e70a45157963dae8eb01bd23b56ecb2
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-01-16 23:04:26 +0900

    모바일사이즈일때 navbar 햄버거버튼처리

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 const header = document.querySelector(".header");
 const headerHeight = header.getBoundingClientRect().height;
+
 const home = document.querySelector(".home__container");
 const homeHeight = home.offsetHeight;
+
+const navbarMenu = document.querySelector(".header__menu");
+const navbarToggle = document.querySelector(".header__toggle");
 
 const navDarkMode = () => {
   // Header에 페이지 아래로 스크롤시 다크 스타일링 적용
@@ -20,8 +24,13 @@ const arrowUpHide = () => {
   if (window.scrollY > homeHeight / 2) arrowUp.style.opacity = 1;
   else arrowUp.style.opacity = 0;
 };
+// Navbar 햄버거 버튼 클릭처ㅣ
+const navbarOnClick = () => {};
 document.addEventListener("scroll", () => {
   navDarkMode();
   homeOpacity();
   arrowUpHide();
+});
+document.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
 });
