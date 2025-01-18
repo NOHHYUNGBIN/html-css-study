44284445357ba8642a80281b08a0c87bb6509eca
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-01-18 19:55:47 +0900

    프로젝트 필터링처리

---
+++ src/projects.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// 프로젝트 필터링 관련 로직 처리
+const categories = document.querySelector(".categories");
+const projects = document.querySelectorAll(".project");
+categories.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter;
+  if (!filter) return;
+  projects.forEach((project) => {
+    if (filter === "all" || filter === project.dataset.type) {
+      project.style.display = "block";
+    } else {
+      project.style.display = "none";
+    }
+  });
+});
