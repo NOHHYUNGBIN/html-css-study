965bd9b1fcc7e11171511a668aead0262df41e45
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-01-18 20:10:43 +0900

    프로젝트 필터링 후 애니메이션 추가 및 엑티브 메뉴 설정

--- src/projects.js
+++ src/projects.js
@@ -3,9 +3,22 @@
 // 프로젝트 필터링 관련 로직 처리
 const categories = document.querySelector(".categories");
 const projects = document.querySelectorAll(".project");
+const projectContainer = document.querySelector(".projects");
+
 categories.addEventListener("click", (e) => {
   const filter = e.target.dataset.filter;
   if (!filter) return;
+  handleActiveSelection(e.target);
+  filterProjects(filter);
+});
+
+const handleActiveSelection = (target) => {
+  const activeMenu = document.querySelector(".category--selected");
+  activeMenu.classList.remove("category--selected");
+  target.classList.add("category--selected");
+};
+
+const filterProjects = (filter) => {
   projects.forEach((project) => {
     if (filter === "all" || filter === project.dataset.type) {
       project.style.display = "block";
@@ -13,4 +26,6 @@ categories.addEventListener("click", (e) => {
       project.style.display = "none";
     }
   });
-});
+  projectContainer.classList.add("anim-out");
+  setTimeout(() => projectContainer.classList.remove("anim-out"), 250);
+};
